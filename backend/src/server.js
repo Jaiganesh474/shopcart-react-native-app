dbbb418bdbb244d77189ad99227fb70acffbb4f2
Jaiganesh474 <jaiganeshrio474@gmail.com>
2025-12-01 13:17:57 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express, { json } from 'express'
+
+const app=express()
+
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({message:"Success"})
+})
+
+app.listen(3000,()=>console.log("Server is up and running"))
