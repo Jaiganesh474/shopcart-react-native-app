7c2cdb013348b29da740f8c8411ccd0bd703ef03
Jaiganesh474 <jaiganeshrio474@gmail.com>
2025-12-01 22:40:37 +0530

    auth implemented

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { StrictMode } from 'react'
 
-createRoot(document.getElementById('root')).render(
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
