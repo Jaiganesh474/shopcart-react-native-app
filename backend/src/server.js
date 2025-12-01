1a11faa2f2bfde9b60c33e24a326cab64c863c7e
Jaiganesh474 <jaiganeshrio474@gmail.com>
2025-12-01 20:11:14 +0530

    startServer

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,9 +25,12 @@ if (ENV.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
-app.listen(ENV.PORT,()=>{
+
+const startServer = async () => {
+  await connectDB();
   app.listen(ENV.PORT, () => {
     console.log("Server is up and running");
   });
-  connectDB();
-})
+};
+
+startServer();
